fb217fb0f1b6b86ca3079e3f921a165d36d33c08
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2020-12-14 23:41:21 -0300

    model sendo feito

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // analisa a requisição e cria o objeto req.bpdy
 app.use(bodyParser.urlencoded({ extended:true}));
 
+
+
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("conectado ao banco!")
+    })
+    .catch(err => {
+        console.log("Não conseguiu se conectar ao banco!", err);
+        process.exit();
+    });
+
+
+
+
+
 app.get("/", (req,res) => {
     res.json({ message: "Servidor funcionando, aguardando rotas"});
 });
